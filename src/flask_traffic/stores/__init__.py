59748da3fff8d109c0506824a84512a7f472efc4
David Carmichael <carmichaelits@gmail.com>
2025-01-06 10:51:11 +0000

    add RedisStore to package

--- src/flask_traffic/stores/__init__.py
+++ src/flask_traffic/stores/__init__.py
@@ -6,5 +6,13 @@ from .csv_store import CSVStore
 from .json_store import JSONStore
 from .sql_orm_store import SQLORMStore, SQLORMModelMixin
 from .sql_store import SQLStore
+from .redis_store import RedisStore
 
-__all__ = ["JSONStore", "CSVStore", "SQLStore", "SQLORMStore", "SQLORMModelMixin"]
+__all__ = [
+    "JSONStore",
+    "CSVStore",
+    "SQLStore",
+    "SQLORMStore",
+    "SQLORMModelMixin",
+    "RedisStore",
+]
